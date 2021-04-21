1e0f9977c0f58fb3a7d60e417efbdfde20d3e937
Max Mendes <maxmendes0509@gmail.com>
2021-04-21 18:30:10 -0300

    new post screen

--- doei-app/View/CreatePostViewController.swift
+++ doei-app/View/CreatePostViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class CreatePostViewController: UIViewController {
     
-    let logado = false
+    
+    @IBOutlet weak var addPhotoView: UIView!
+    @IBOutlet weak var mainView: UIView!
+    
+    let logado = true
     override func viewDidLoad() {
         super.viewDidLoad()
+        addPhotoView.addDashedBorder()
         
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("TOCOU")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,5 +37,26 @@ class CreatePostViewController: UIViewController {
             loginVC.modalPresentationStyle = .popover
             tabBarController?.present(loginVC, animated: true, completion: nil)
         }
+    }
+}
+
+extension UIView {
+    func addDashedBorder() {
+        let color = UIColor(red: 1/255, green: 102/255, blue: 92/255, alpha:  1).cgColor
+        
+        let shapeLayer:CAShapeLayer = CAShapeLayer()
+        let frameSize = self.frame.size
+        let shapeRect = CGRect(x: 0, y: 0, width: frameSize.width - 40, height: frameSize.height)
+        
+        shapeLayer.bounds = shapeRect
+        shapeLayer.position = CGPoint(x: frameSize.width/2 - 20, y: frameSize.height/2)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = color
+        shapeLayer.lineWidth = 2
+        shapeLayer.lineJoin = CAShapeLayerLineJoin.round
+        shapeLayer.lineDashPattern = [6,3]
+        shapeLayer.path = UIBezierPath(roundedRect: shapeRect, cornerRadius: 5).cgPath
+        
+        self.layer.addSublayer(shapeLayer)
     }
 }
